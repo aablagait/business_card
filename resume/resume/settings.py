8ba5548605d3a2b9d253ea72153fc5834d387eb2
root <root@3111631-et13451.twc1.net>
2024-07-06 23:48:05 +0300

    развернутый проект на сервере

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -8,10 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['91.200.148.236', '127.0.0.1', 'localhost','aablagait.servehttp.com', ]
 
 
 # Application definition
@@ -107,6 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static_dev',
